972d1b0529a0545219ce35eac72d12dddc71c515
Christie.Kh <kharushina.christina@gmail.com>
2018-10-15 21:07:58 +0500

    Updates

---
+++ precache-manifest.f7bfaf701ccdd47d9c9cbd5b233afe67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bb688e992edd132c439",
+    "url": "/test-employees/static/js/runtime~main.6bb688e9.js"
+  },
+  {
+    "revision": "4ff47b8d05e86d26102f",
+    "url": "/test-employees/static/js/main.4ff47b8d.chunk.js"
+  },
+  {
+    "revision": "29b7954b9b8e5e159a05",
+    "url": "/test-employees/static/js/1.29b7954b.chunk.js"
+  },
+  {
+    "revision": "4ff47b8d05e86d26102f",
+    "url": "/test-employees/static/css/main.55b2d2c8.chunk.css"
+  },
+  {
+    "revision": "7ec3d871187de7af3b528f7a80e75bd1",
+    "url": "/test-employees/index.html"
+  }
+];
